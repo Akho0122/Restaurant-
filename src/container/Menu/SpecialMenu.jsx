e06857541c68ee27879f552f1fa4748c8c4321e3
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-24 21:20:25 +0500

    Special Menu style

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -11,10 +11,15 @@ const SpecialMenu = () => (
     </div>
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
           ))}
         </div>
       </div>
@@ -22,10 +27,15 @@ const SpecialMenu = () => (
         <img src={images.menu} alt="menu-img" />
       </div>
       <div className="app__specialMenu-menu_coctails flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
           ))}
         </div>
       </div>
