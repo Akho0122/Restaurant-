f45e385690a8f82f6c745623edeade1b8d62c8c9
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-25 22:15:48 +0500

    Laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
-import './Laurels.css';
+import React from "react";
+import { images, data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
+import "./Laurels.css";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
